7938e41a4c0c04e3189b2e61fbc4de8601509fdb
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-11-02 19:47:27 -0700

    new database

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     client: "postgresql",
     connection:
-      "postgres://irsadmqz:BV97_ZlARjbZDzVV7_D8o_BJhGmIiaO1@kashin.db.elephantsql.com:5432/irsadmqz",
+      "postgres://tvfhobcu:clZojJOZMSue5OtI_h9txf9p5JsHEfhm@fanny.db.elephantsql.com/tvfhobcu",
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
